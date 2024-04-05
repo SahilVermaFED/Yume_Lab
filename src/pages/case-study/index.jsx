cf7a4a4681fe8c4f9dc1ea1bcfee909142170578
Sahil Verma <sahilverma15012002@gmail.com>
2024-04-05 12:27:58 +0530

    [New} feature

---
+++ src/pages/case-study/index.jsx
@@ -0,0 +1,19 @@
+import Strategy from '../../pages/case-study/Strategy/index';
+import HeroSection from './HeroSection';
+import StatementSection from './StatementSection';
+import Unbiased from './Unbiased';
+import Project from '../../pages/case-study/Project/index';
+
+const CaseStudy = () => {
+    return (
+        <>
+            <HeroSection />
+            <StatementSection />
+            <Strategy />
+            <Unbiased />
+            <Project />
+        </>
+    );
+};
+
+export default CaseStudy;
